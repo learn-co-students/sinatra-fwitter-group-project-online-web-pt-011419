04cc5884dcdc07d5426a5146f3a9af7af8d5edb2
github username <github email address>
2019-04-30 13:28:51 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,61 +13,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    if !logged_in?
-      erb :'/users/create_user'
-    else
-      redirect to '/tweets/tweets'
-    end
-  end
-
-  post '/signup' do
-    if params[:username].empty? || params[:email].empty? || params[:password].empty?
-      redirect to '/signup'
-    else
-      @user = User.create(username: params[:username], password: params[:password])
-      @user.save
-      session[:user_id] = @user.id
-      redirect to '/tweets/tweets'
-    end
-  end
-
-  get '/tweets/tweets' do
-    if logged_in?
-      @tweets = Tweet.all
-      erb :'/tweets/tweets'
-    else
-      redirect to '/login'
-    end
-  end
-
-  get '/login' do
-    if !logged_in?
-      erb :'/users/login'
-    else
-      redirect to '/tweets/tweets'
-    end
-  end
-
-  post '/login' do
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to '/tweets/tweets'
-    else
-      redirect to '/signup'
-    end
-  end
-
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect to '/login'
-    else
-      redirect to '/'
-    end
-  end
-
   helpers do
     def logged_in?
       !!current_user
